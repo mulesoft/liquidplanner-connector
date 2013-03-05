506030ac433753cf49bc67b1b391154bf642e542
Damian Sima <damiansima@gmail.com>
2013-03-05 16:12:50 -0300

    remove credentials from the integration test and add a property file

--- service-client/src/test/java/org/mule/LiquidPlanner/integration/client/core/AbstractServiceClientTestIT.java
+++ service-client/src/test/java/org/mule/LiquidPlanner/integration/client/core/AbstractServiceClientTestIT.java
@@ -1,8 +1,30 @@
 package org.mule.LiquidPlanner.integration.client.core;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public abstract class AbstractServiceClientTestIT {
-    protected static final String USER = "damian.sima@mulesoft.com";
-    protected static final String PASSWORD = "MuleMadman";
+    private static final String LP_USER_KEY = "lp.user";
+    private static final String LP_PASSWORD_KEY = "lp.password";
+
+    protected static String USER;
+    protected static String PASSWORD;
+
+    public AbstractServiceClientTestIT() {
+        Properties properties = new Properties();
+        InputStream stream = Thread.currentThread().getContextClassLoader()
+                .getResourceAsStream("test_credentials.properties");
+
+        try {
+            properties.load(stream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        USER = properties.getProperty(LP_USER_KEY);
+        PASSWORD = properties.getProperty(LP_PASSWORD_KEY);
+    }
 
     protected void printOutResponse(String response) {
         System.out.println(response);
